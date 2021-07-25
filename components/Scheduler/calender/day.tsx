c00e97d32716cb5843af8ad1d07c3167df7b8a4a
steven <stv.chm@gmail.com>
2021-07-26 01:41:52 +0800

    [snap]

---
+++ components/Scheduler/calender/day.tsx
@@ -0,0 +1,119 @@
+import React from "react";
+import {  Column, Tile, SelectableTile } from "carbon-components-react";
+import styles from './calender.module.scss';
+import { DateTime } from "luxon";
+import { useLongPress } from "react-use"
+
+
+
+export type IDayCellEvent = (event: MouseEvent | TouchEvent, day: DateTime) => any | void;
+export type ITaskEvent = (event: MouseEvent | TouchEvent, node: ITaskNode) => any | void;
+
+
+export interface ITaskNode {
+  id: Number,
+  project_id: string,
+  title: string,
+  startTime: DateTime,
+  endTime: DateTime,
+}
+
+
+export interface IDayCellItem {
+  isSelected: boolean,
+  isToday: boolean,
+  inMonth: boolean,
+  date: DateTime,
+  events: Array<ITaskNode>,
+}
+export interface IDayCellProps {
+  dateItem: IDayCellItem,
+  isSelectable: boolean,
+  onDayCellClicked: IDayCellEvent,
+  onDayCellLongPressed: IDayCellEvent,
+  onTaskClicked: ITaskEvent,
+}
+
+const renderDayCell = (props: IDayCellProps) => {
+  const { date, isToday, inMonth } = props.dateItem;
+  const idKey = date.toFormat('yy-mm-dd');
+  const labelClass = `${styles.day_label} ${isToday ? styles.is_today : ''}`;
+  const tileClass = `${styles.day_tile}  ${(!inMonth ? styles.not_in_month : '')} `;
+
+  // console.log({ labelClass, dateItem });
+  // long-press event 
+  const onLongPress = (e: MouseEvent | TouchEvent) => props.onDayCellLongPressed(e, date);
+  const longPressEvent = useLongPress(onLongPress, {
+    isPreventDefault: true,
+    delay: 900,
+  });
+  // 
+
+  // PC-mouse-hover : show-Date 
+
+  //
+  return (
+    <Column className={`${styles.day_cell} day-${idKey} bx--aspect-ratio bx--aspect-ratio--4x3`} >
+
+      {!props.isSelectable &&
+        <Tile
+          id={idKey}
+          className={tileClass}
+          onClick={(e: any) => props.onDayCellClicked(e, date)}
+          {...longPressEvent}
+        >
+          <span className={labelClass}> {`${date.day}`} </span>
+          <div className={styles.task_list}>
+            {props.dateItem.events.slice(0, 4).map((node: ITaskNode) =>
+              <div
+                id={`task-id-${node.id}`}
+                className={`task-${node.id} ${styles.task_item}`}
+                key={`task-${node.id}`}
+                onClick={(e: any) => props.onTaskClicked(e as MouseEvent, node)}
+              >
+              </div>
+
+            )}
+            {(props.dateItem.events.length > 4) &&
+              <span className={styles.task_more_item}
+                onClick={(e: any) => props.onTaskClicked(e as MouseEvent, props.dateItem.events[5])} >
+                more+ {props.dateItem.events.length - 4}
+              </span>
+            }
+          </div>
+        </Tile>
+      }
+      {props.isSelectable &&
+        <SelectableTile
+          id={idKey}
+          className={tileClass}
+          value={date.toFormat('yy-mm-dd')}
+          onClick={(e: any) => props.onDayCellClicked(e, date)}
+        >
+          <span className={labelClass}> {`${date.day}`} </span>
+          <div className={styles.task_list}>
+            {props.dateItem.events.slice(0, 4).map((node: ITaskNode) =>
+              <div
+                id={`task-id-${node.id}`}
+                className={`task-${node.id} ${styles.task_item}`}
+                key={`task-${node.id}`}
+                onClick={(e: any) => props.onTaskClicked(e as MouseEvent, node)}
+              >
+              </div>
+
+            )}
+            {(props.dateItem.events.length > 4) &&
+              <span className={styles.task_more_item}
+                onClick={(e: any) => props.onTaskClicked(e as MouseEvent, props.dateItem.events[5])} >
+                more+ {props.dateItem.events.length - 4}
+              </span>
+            }
+          </div>
+        </SelectableTile>
+      }
+    </Column>
+  )
+}
+
+const DayCellColumn = renderDayCell;
+export default DayCellColumn;
--- pages/schedules/index.tsx
+++ pages/schedules/index.tsx
@@ -5,11 +5,65 @@ import useSWR from 'swr';
 import { Trello } from '@/interfaces/Trello';
 import NavLayout from '@/components/Layouts/index';
 import SchedulerComponent from '@/components/Scheduler/application';
+import {ITaskNode} from '@/components/Scheduler/calender';
 
-
+import { DateTime } from "luxon";
 const scheduleList = () => {
+  const taskList = [
+    {
+      id: 1, 
+      title : 'work out',
+      project_id : 'sdkjf1',
+      startTime: DateTime.fromFormat('2021-07-21' , 'yyyy-MM-dd'),
+      endTime: DateTime.fromFormat('2021-07-28', 'yyyy-MM-dd'),
+    },
+    {
+      id: 2,
+      title: 'work out',
+      project_id: 'sdkjf1',
+      startTime: DateTime.fromFormat('2021-07-21', 'yyyy-MM-dd'),
+      endTime: DateTime.fromFormat('2021-07-28', 'yyyy-MM-dd'),
+    },
+    {
+      id: 3,
+      title: 'work out',
+      project_id: 'sdkjf1',
+      startTime: DateTime.fromFormat('2021-07-21', 'yyyy-MM-dd'),
+      endTime: DateTime.fromFormat('2021-07-28', 'yyyy-MM-dd'),
+    },
+    {
+      id: 4,
+      title: 'work out',
+      project_id: 'sdkjf1',
+      startTime: DateTime.fromFormat('2021-07-21', 'yyyy-MM-dd'),
+      endTime: DateTime.fromFormat('2021-07-28', 'yyyy-MM-dd'),
+    },
+    {
+      id: 5,
+      title: 'work out',
+      project_id: 'sdkjf1',
+      startTime: DateTime.fromFormat('2021-07-21', 'yyyy-MM-dd'),
+      endTime: DateTime.fromFormat('2021-07-28', 'yyyy-MM-dd'),
+    },
+    {
+      id: 6,
+      title: 'work out',
+      project_id: 'sdkjf1',
+      startTime: DateTime.fromFormat('2021-07-21', 'yyyy-MM-dd'),
+      endTime: DateTime.fromFormat('2021-07-28', 'yyyy-MM-dd'),
+    },
+    {
+      id: 7,
+      title: 'work out',
+      project_id: 'sdkjf1',
+      startTime: DateTime.fromFormat('2021-07-21', 'yyyy-MM-dd'),
+      endTime: DateTime.fromFormat('2021-07-28', 'yyyy-MM-dd'),
+    },
+
+
+  ] as Array<ITaskNode>
   return (
-    <SchedulerComponent taskList={[]} />
+    <SchedulerComponent taskList={taskList} />
   );
 }
 
